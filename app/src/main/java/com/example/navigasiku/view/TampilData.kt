e9d7a125391619aad93d5e69be8bb8631fa43a4e
nayisntaround87 <naylasalwahayati@gmail.com>
2025-11-06 16:15:06 +0700

    membuat val items

--- app/src/main/java/com/example/navigasiku/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -2,9 +2,15 @@ package com.example.navigasiku.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Tampildata(
-
-)
+    onBackBtnClick:()-> Unit
+){
+    val items = listOf(
+        Pair(first = stringResource(R.string.nama_lengkap), second = "Contoh Nama"),
+        Pair(first = stringResource(R.string.jenis_kelamin), second = "Lainnya"),
+        Pair(first = stringResource(R.string.alamat), second = "Yogyakarta")
+    )
