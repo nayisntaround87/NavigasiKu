1e88c3a2606c460c81f5e572a2b8e549d475ec2a
nayisntaround87 <naylasalwahayati@gmail.com>
2025-11-06 16:01:28 +0700

    membuat horizontal divider dan outline text field

--- app/src/main/java/com/example/navigasiku/view/Formulir.kt
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -71,3 +71,17 @@ fun FormIsian(
                     }
                 }
             }
+            HorizontalDivider(modifier = Modifier
+                .padding(all = 20.dp)
+                .width(width = 250.dp),
+                thickness = 1.dp,
+                color = Color.Red)
+
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .width(width = 250.dp),
+                label = {Text(text = "Alamat")},
+                onValueChange = {},
+            )
