cf77f30550d349a0c39f3ceb63bd87832c2c7fa7
nayisntaround87 <naylasalwahayati@gmail.com>
2025-11-06 16:11:03 +0700

    membuat fun Tampildata

--- app/src/main/java/com/example/navigasiku/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -1,5 +1,10 @@
 package com.example.navigasiku.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Tampildata(
+
+)
