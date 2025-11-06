b5a818442cc5e07153768311f7f60831cb5d81a9
nayisntaround87 <naylasalwahayati@gmail.com>
2025-11-06 15:58:25 +0700

    membuat scaffold

--- app/src/main/java/com/example/navigasiku/view/Formulir.kt
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -1,28 +1,45 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.navigasiku.view
 
-import android.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.navigasiku.R
+
 
 @Composable
 fun FormIsian(
-    jenisK : List<String> = listOf("Laki-laki", "Pertemuan"),
+    jenisK : List<String> = listOf("Laki-laki","Perempuan"),
     OnSubmitBtnClick : ()-> Unit
 ){
     Scaffold (modifier = Modifier,
         topBar = {
             TopAppBar(
-                title = { Text(text = stringResource(R.string.home),
-                color = Color.White)},
+                title = { Text(text = stringResource(id = R.string.home),
+                    color = Color.White)},
                 colors = TopAppBarDefaults.
-            topAppBarColors(containerColor = colorResource(R.color.teal_700))
-            ){}
-
-}
+                topAppBarColors(containerColor = colorResource(id = R.color.teal_700))
+            ) }
