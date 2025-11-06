e301466adbde27b4b15a8b6d8857b27005541dbe
nayisntaround87 <naylasalwahayati@gmail.com>
2025-11-06 15:52:54 +0700

    membuat scaffold

--- app/src/main/java/com/example/navigasiku/view/Formulir.kt
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -1,9 +1,28 @@
 package com.example.navigasiku.view
 
 import android.R
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun FormIsian(
-    jenisK : List<string>
-)
+    jenisK : List<String> = listOf("Laki-laki", "Pertemuan"),
+    OnSubmitBtnClick : ()-> Unit
+){
+    Scaffold (modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(R.string.home),
+                color = Color.White)},
+                colors = TopAppBarDefaults.
+            topAppBarColors(containerColor = colorResource(R.color.teal_700))
+            ){}
+
+}
