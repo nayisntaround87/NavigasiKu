fc73608a2b8433d628a7d20aed3709e8e66002ed
nayisntaround87 <naylasalwahayati@gmail.com>
2025-11-06 16:19:01 +0700

    membuat scaffold

--- app/src/main/java/com/example/navigasiku/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -1,7 +1,10 @@
 package com.example.navigasiku.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -14,3 +17,10 @@ fun Tampildata(
         Pair(first = stringResource(R.string.jenis_kelamin), second = "Lainnya"),
         Pair(first = stringResource(R.string.alamat), second = "Yogyakarta")
     )
+    Scaffold (modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.tampil),color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id = R.color.teal_700))
+            )
+
