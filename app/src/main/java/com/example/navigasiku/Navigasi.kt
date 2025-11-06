a2d7971f80e0b1aba768d07a9bfc2d837fcbc91a
nayisntaround87 <naylasalwahayati@gmail.com>
2025-11-06 15:25:58 +0700

    membuat composable data app

--- app/src/main/java/com/example/navigasiku/Navigasi.kt
+++ app/src/main/java/com/example/navigasiku/Navigasi.kt
@@ -1,7 +1,30 @@
 package com.example.navigasiku
 
+import android.widget.MediaController
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
 enum class Navigasi{
     Formulir,
 
     Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+
+    modifier: Modifier
+){
+    Scaffold {isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+
+            modifier = Modifier.padding(isiRuang)
+        ){
+
+        }
 }
